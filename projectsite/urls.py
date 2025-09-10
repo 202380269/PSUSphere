a50c4e69690d3b5db6b7fdbd2bde28b967f2a397
John Christopher Remandaban <202380127@psu.palawan.edu.ph>
2025-09-10 13:55:40 +0800

    modify urls.py

--- projectsite/urls.py
+++ projectsite/urls.py
@@ -17,15 +17,19 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
-
+from studentorg.views import (
+    HomePageView,
+    OrganizationList,
+    OrganizationCreateView,
+    OrganizationUpdateView,
+    OrganizationDeleteView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),  # Home page
-    path('organization_list', OrganizationList.as_view(), name='organization-list'),
-    path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
-    path('organization_list/<pk>', OrganizationUpdateView.as_view(), name='organization-update'),
-    path('organization_list/<pk>/delete', OrganizationDeleteView.as_view(), name='organization-delete'),
-
+    path('organization_list/', OrganizationList.as_view(), name='organization-list'),
+    path('organization_list/add/', OrganizationCreateView.as_view(), name='organization-add'),
+    path('organization_list/<int:pk>/', OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/<int:pk>/delete/', OrganizationDeleteView.as_view(), name='organization-delete'),
 ]
