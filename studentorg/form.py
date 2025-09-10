79c591699bd6ef13600137be943c7f9b58d384d4
John Christopher Remandaban <202380127@psu.palawan.edu.ph>
2025-09-10 14:40:28 +0800

    modify form.py

--- studentorg/form.py
+++ studentorg/form.py
@@ -1,9 +1,27 @@
-from django.forms import ModelForm
 from django import forms
-from .models import Organization
+from .models import Organization, College, Program, Student, OrgMember
 
-
-class OrganizationForm(ModelForm):
+class OrganizationForm(forms.ModelForm):
     class Meta:
         model = Organization
-        fields = "__all__"
+        fields = '__all__'
+
+class CollegeForm(forms.ModelForm):
+    class Meta:
+        model = College
+        fields = '__all__'
+
+class ProgramForm(forms.ModelForm):
+    class Meta:
+        model = Program
+        fields = '__all__'
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+class OrgMemberForm(forms.ModelForm):
+    class Meta:
+        model = OrgMember
+        fields = '__all__'
