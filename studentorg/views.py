83c23b268994d7504f732d8d1b2d634b016bd4af
John Christopher Remandaban <202380127@psu.palawan.edu.ph>
2025-09-15 02:49:09 +0800

    modify view.py

--- studentorg/views.py
+++ studentorg/views.py
@@ -6,24 +6,28 @@ from studentorg.forms import OrganizationForm, CollegeForm, ProgramForm, Student
 from django.urls import reverse_lazy
 from django.utils import timezone
 
+
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
-def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    context["total_students"] = Student.objects.count()
-    
-    today = timezone.now().date()
-    count = (
-        OrgMember.objects.filter(date_joined__year=today.year)
-        .values("student")
-        .distinct()
-        .count()
-    )
-    context["students_joined_this_year"] = count
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["total_students"] = Student.objects.count()
+        context["total_organizations"] = Organization.objects.count()
 
-    return context
+        today = timezone.now().date()
+        count = (
+            OrgMember.objects.filter(
+                date_joined__year=today.year
+            )
+            .values("student")
+            .distinct()
+            .count()
+        )
+
+        context["students_joined_this_year"] = count
+        return context
 
 
 class OrganizationList(ListView):
