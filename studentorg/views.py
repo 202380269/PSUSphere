a986ca10f6b80cf08748f892041ecd7288a7cf5a
John Christopher Remandaban <202380127@psu.palawan.edu.ph>
2025-09-15 00:18:13 +0800

    modify views.py

--- studentorg/views.py
+++ studentorg/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.list import ListView
-
+from django.db.models import Q
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization, College, Program, Student, OrgMember
 from studentorg.forms import OrganizationForm, CollegeForm, ProgramForm, StudentForm, OrganizationMemberForm
@@ -15,6 +15,18 @@ class OrganizationList(ListView):
     context_object_name = 'organization'
     template_name = 'org_list.html'
     paginate_by = 5
+    
+def get_queryset(self):
+    qs = super().get_queryset()  
+    query = self.request.GET.get('q')  
+
+    if query:
+        qs = qs.filter(
+            Q(name__icontains=query) |
+            Q(description__icontains=query)
+        )
+    return qs
+
 
 class OrganizationCreateView(CreateView):
     model = Organization
