fa1b21e07b9ee88cfc6fbcea7ca6b0a45fc410b6
John Christopher Remandaban <202380127@psu.palawan.edu.ph>
2025-09-15 08:47:00 +0800

    modify setting.py

--- projectsite/settings.py
+++ projectsite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7qyop6j+b3)z(39!-rhuwfug-sb89p1n=r@s*eq=x@(c_h6ka$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ayrong69.pythonanywhere.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['JCBRON.pythonanywhere.com','localhost', '127.0.0.1']
 
 # Application definition
 
@@ -146,12 +146,12 @@ STATIC_URL = 'static/'
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_URL = '/accounts/login/' # where @login_required will send users
-LOGIN_REDIRECT_URL = '/' # where to go after successful login
-LOGOUT_REDIRECT_URL = '/accounts/login/' # after logout, go back to login
-ACCOUNT_LOGOUT_REDIRECT_URL = '/' # where to redirect after logout
-ACCOUNT_LOGOUT_ON_GET = True # logout immediately on GET
-ACCOUNT_LOGIN_METHODS = {"username", "email"} # allow login with username OR email
+LOGIN_URL = '/accounts/login/' 
+LOGIN_REDIRECT_URL = '/' 
+LOGOUT_REDIRECT_URL = '/accounts/login/' 
+ACCOUNT_LOGOUT_REDIRECT_URL = '/' 
+ACCOUNT_LOGOUT_ON_GET = True 
+ACCOUNT_LOGIN_METHODS = {"username", "email"} 
 ACCOUNT_SIGNUP_FIELDS = [
 "username*",
 "email*",
